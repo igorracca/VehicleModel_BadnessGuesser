2804ebd695dbb3a6e0ce40ea4fd4e84be90474cf
igorracca <igorracca13@gmail.com>
2020-05-02 19:08:47 +0200

    fixing py file

--- CarScan_NN/CarBadness_NN.py
+++ CarScan_NN/CarBadness_NN.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 
 device = torch.device("cuda:0")
 
-def read_data(base_path='./Data',split_size=[0.8, 0.1, 0.1]):  #  the split_size gives how much of the data goes to the train/test.
+def read_data(base_path='./Data',split_size=[0.6, 0.2, 0.2]):  #  the split_size gives how much of the data goes to the train/test.
     """
     Reads all of the .mat files in the given base_path, and returns a dict with the data found there.
     :param split_size:
@@ -44,13 +44,13 @@ def read_data(base_path='./Data',split_size=[0.8, 0.1, 0.1]):  #  the split_size
     for key in data_dict.keys():
         new_data.append(data_dict[key])
     np.random.shuffle(new_data)
-    if isinstance(split_size, tuple):
+    if isinstance(split_size, list):
         training_samples = int(split_size[0] * len(new_data))
         valid_samples = int(split_size[1] * len(new_data))
         test_samples = int(split_size[2] * len(new_data))
         while sum([training_samples, valid_samples, test_samples]) != len(new_data):
             training_samples += 1
-
+        #split samples
         new_datadict = {'train': new_data[:training_samples],
                         'validation': new_data[training_samples + 1:training_samples + valid_samples],
                         'test': new_data[-test_samples:]}
@@ -61,8 +61,8 @@ def read_data(base_path='./Data',split_size=[0.8, 0.1, 0.1]):  #  the split_size
     print("Adatbetöltés kész")
     return new_datadict
 
-class CarBadnessGuesser(nn.Module):
-    def __init__(self, lr=0.0001):
+    class CarBadnessGuesser(nn.Module):
+    def __init__(self, lr=0.01):
         super(CarBadnessGuesser, self).__init__()
 
         self.dataset = read_data()
@@ -76,7 +76,8 @@ class CarBadnessGuesser(nn.Module):
             nn.Conv3d(in_channels=2, out_channels=1, kernel_size=3),
             nn.BatchNorm3d(1),
             nn.AdaptiveMaxPool3d((1, 1, 10)),
-        )
+        ) 
+        
         self.linear = nn.Sequential(
             nn.Linear(in_features=10, out_features=5),
             nn.ReLU(),
@@ -90,6 +91,23 @@ class CarBadnessGuesser(nn.Module):
         self.loss_fn = nn.MSELoss()
         self.optimizer = optim.Adam(list(self.model.parameters()) + list(self.linear.parameters()), lr=lr)
 
+
+    # 0: no damage, 1: slightly damaged, 2: damaged, 3: very damaged
+    def getClass(self, x):
+        if x==0:
+            return 0      
+        x = int(x)
+        if x<=100/3:
+            return 1
+        elif  x>100/3 and x<200/3:
+            return 2
+        else:
+            return 3
+    
+    # compare prediction and label classes
+    def isCorrect(self, prediction, label):
+        return self.getClass(prediction) == self.getClass(label)
+
     def forward(self, x):
         conv_out = self.model(x.unsqueeze(0).unsqueeze(0))
         return self.linear(conv_out.squeeze(-2).squeeze(-2))
@@ -97,24 +115,43 @@ class CarBadnessGuesser(nn.Module):
     def train(self, epochs=50):
         b_loss = []
         v_loss = []
+    
         for epoch in trange(epochs):
+            t_correct = 0
+            total = 1
             for step, data in enumerate(self.dataset["train"]):
                 input_data = data['KUL'].cuda()
+                label = data['EES'].cuda()
+                
                 prediction = self(input_data)
-                loss = self.loss_fn(prediction, data['EES'].cuda())
-                loss.backward()
+                loss = self.loss_fn(prediction, label)
+                #print('training- ', 'y^', prediction.item(), 'y', label.item(), loss.item())
+                #print('t_correct', t_correct, 'total',total)
+                
+                loss.backward()     
                 self.optimizer.step()
-                self.zero_grad()
                 b_loss.append(loss.item())
-                #  print(f'Batch loss: {loss.item()}', flush=True)
+                
+                #check if its prediction matches label class
+                if( self.isCorrect(prediction.item(), label.item()) ):
+                    t_correct += 1;
+                total += 1;
+                
+                self.zero_grad()
             if epoch % self.valid_freq and epoch != 0:
-                print(f'Validation loss: {self.validation()}', flush=True)
-                v_loss.append(self.validation())
-        print("Train is complete")
+                #calculate the training accuracy
+                t_acc = t_correct/total * 100
+                #print(f'Batch loss: {loss.item()}', flush=True)
+             
+                vloss, v_acc = self.validation()
+                v_loss.append(vloss)
+                print(f'Validation loss:', vloss, flush=True)
 
-        self.test()
+                
+        print('Training acc:', t_acc, '%')
+        print('Validation acc: ', v_acc, '%')  
         self.save_weights()
-        #  self.save_checkpoint()
+        
         plt.plot(v_loss)
         plt.ylabel('Validation loss')
         plt.show()
@@ -127,15 +164,32 @@ class CarBadnessGuesser(nn.Module):
         Runs the validation phase of the training
         :return: The validation loss average
         """
+        v_correct = 0
+        total = 1
         average_loss = 0
         step = 0
+        
         for step, data in enumerate(self.dataset['validation']):
             with torch.no_grad():
                 input_data = data['KUL'].cuda()
+                label = data['EES'].cuda()
+                
                 prediction = self(input_data)
-                loss = self.loss_fn(prediction, data['EES'].cuda())
+                loss = self.loss_fn(prediction, label)               
+                #print('validation- ', 'y^', prediction.item(), 'y', label.item(), loss.item())
+                #print('v_correct', v_correct, 'total',total)
+                
                 average_loss += loss.item()
-        return average_loss / (step + 1)
+                
+                #check if its correct
+                c = self.isCorrect(prediction.item(), label.item())
+                if(c == True):
+                    v_correct += 1;
+                total += 1;
+        
+        #calculate the validation accuracy
+        v_correct = v_correct/total * 100  
+        return average_loss / (step + 1), v_correct
         print("Validation is complete")
 
     def test(self):
@@ -165,32 +219,13 @@ class CarBadnessGuesser(nn.Module):
             os.makedirs(save_path)
         torch.save(list(self.model.parameters()) +
                    list(self.linear.parameters()), os.path.join(save_path, 'model.weights'))
-        print("saveing weights is complete")
-
-    # def save_checkpoint(self):
-    #     checkpoint = {'model': self.model,
-    #      'state_dict': self.model.state_dict(),
-    #      'optimizer' : self.optimizer.state_dict()}
-    #    torch.save(checkpoint, 'checkpoint.pth')
-    #     print("saveing checkpoint is complete")
+        print("saving weights is complete")
 
     def load_weights(self):
         self.optimizer = torch.load('model.weights')
         print("loading is complete")
 
-    # def load_checkpoint(self, filepath):
-    #    checkpoint = torch.load(self, filepath)
-    #     self.model = checkpoint['model']
-    #     self.model.load_state_dict(checkpoint['state_dict'])
-    #     for parameter in self.model.parameters():
-    #         parameter.requires_grad = False
-    #     self.model.eval()
-    #     return self.model
-
 if __name__ == "__main__":
     learner = CarBadnessGuesser()
     torch.backends.cudnn.enabled = False
     learner.train()
-#    learner.model = learner.load_checkpoint('checkpoint.pth')
-#    learner.load_weights()
-#    learner.forward()
